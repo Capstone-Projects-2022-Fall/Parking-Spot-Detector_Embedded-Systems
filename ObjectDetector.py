f5c0d6a529401e1b14f26f576c4686ae3eef1649
HHasib <hasib1220@gmail.com>
2022-10-03 01:58:38 -0400

    Initial successful Object Detecttion Code

---
+++ ObjectDetector.py
@@ -0,0 +1,96 @@
+import argparse
+import sys
+import time
+import cv2
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
+import utils
+import os
+
+
+def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
+        enable_edgetpu: bool) -> None:
+
+    # Initialize the object detection model
+    base_options = core.BaseOptions(
+      file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
+    detection_options = processor.DetectionOptions(
+      max_results=3, score_threshold=0.3)
+    options = vision.ObjectDetectorOptions(
+      base_options=base_options, detection_options=detection_options)
+    detector = vision.ObjectDetector.create_from_options(options)
+
+    input_path = r'input/tftest.png'
+    image = cv2.imread(input_path)
+
+    # Convert the image from BGR to RGB as required by the TFLite model.
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Create a TensorImage object from the RGB image.
+    input_tensor = vision.TensorImage.create_from_array(rgb_image)
+
+    # Run object detection estimation using the model.
+    print("Detection started..")
+    detection_result = detector.detect(input_tensor)
+    print("Detection complete..")
+    # Draw keypoints and edges on input image
+    print("Visualization started..")
+    image = utils.visualize(image, detection_result)
+    print("Visualization complete..")
+
+    text_location = (24, 20)
+    font_size = 1
+    font_thickness = 1
+    font_color = (255,0,0) #red color
+    image = cv2.putText(image, 'Parking Spot Detector', text_location, cv2.FONT_HERSHEY_PLAIN,
+                font_size, font_color, font_thickness)
+    #print("Showing Image now...")
+    #cv2.imshow('Parking Spot Detector: Bye Bye World', image)
+    output_image = 'output/tftestoutput.png'
+    cv2.imwrite(output_image, image)
+    print('Successfully saved')
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model',
+      help='Path of the object detection model.',
+      required=False,
+      default='efficientdet_lite0.tflite')
+  parser.add_argument(
+      '--cameraId', help='Id of camera.', required=False, type=int, default=0)
+  parser.add_argument(
+      '--frameWidth',
+      help='Width of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=640)
+  parser.add_argument(
+      '--frameHeight',
+      help='Height of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=480)
+  parser.add_argument(
+      '--numThreads',
+      help='Number of CPU threads to run the model.',
+      required=False,
+      type=int,
+      default=4)
+  parser.add_argument(
+      '--enableEdgeTPU',
+      help='Whether to run the model on EdgeTPU.',
+      action='store_true',
+      required=False,
+      default=False)
+  args = parser.parse_args()
+
+  run(args.model, int(args.cameraId), args.frameWidth, args.frameHeight,
+      int(args.numThreads), bool(args.enableEdgeTPU))
+
+
+if __name__ == '__main__':
+  main()
